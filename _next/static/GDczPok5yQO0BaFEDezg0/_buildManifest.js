e20764289e6dcbb27d2309039b18157eb137aa05
link-forte <link-forte@users.noreply.github.com>
2022-06-04 13:44:47 +0000

    deploy: 33d8e8c38b72de40c11438959fa34b6bf3f07f8b

--- _next/static/GDczPok5yQO0BaFEDezg0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d90085ecb74607df.js"],"/_error":["static/chunks/pages/_error-635a6533d6ac3ca1.js"],"/linkSample":["static/chunks/pages/linkSample-5b9233e4b40415c4.js"],sortedPages:["/","/_app","/_error","/linkSample"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
